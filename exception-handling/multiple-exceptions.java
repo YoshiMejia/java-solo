e480a70afdb53848a3fada972f506200759de4db
Yoshi <yoshimejia00@gmail.com>
2022-10-26 22:56:21 -0400

    solved

---
+++ exception-handling/multiple-exceptions.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
+public class Main
+{
+	public static void main(String[] args) {
+	    
+	    Scanner scanner = new Scanner(System.in);
+	    
+	    try {
+	        int num1 = scanner.nextInt();
+	        int num2 = scanner.nextInt();
+	        /*
+	        1. Error: division by zero
+	        2. Error: wrong value type
+	        */
+	        //your code goes here
+			System.out.println(num1/num2);
+	    } catch(ArithmeticException e1) {
+	        System.out.println("Error: division by zero");
+	    } catch(InputMismatchException e1){
+	        System.out.println("Error: wrong value type");
+	    }
+	}
+}
