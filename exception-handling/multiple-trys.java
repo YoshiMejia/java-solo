ecfef9efc5a93e3025e75c861d4b16890ad7ec2b
Yoshi <yoshimejia00@gmail.com>
2022-10-26 22:56:27 -0400

    solved

---
+++ exception-handling/multiple-trys.java
@@ -0,0 +1,11 @@
+//All catch blocks should be ordered from most specific to most general.
+// Following the specific exceptions, you can use the Exception type to handle all other exceptions as the last catch.
+try {
+    //some code
+  } catch (ExceptionType1 e1) {
+    //Catch block
+  } catch (ExceptionType2 e2) {
+    //Catch block
+  } catch (ExceptionType3 e3) {
+    //Catch block
+  }
