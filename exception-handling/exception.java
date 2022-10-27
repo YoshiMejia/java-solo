df1b98a5edd53484d42c64025d2cecba0dc32d90
Yoshi <yoshimejia00@gmail.com>
2022-10-26 22:34:00 -0400

    comments

--- exception-handling/exception.java
+++ exception-handling/exception.java
@@ -5,13 +5,16 @@ public class Main
 	public static void main(String[] args) {
 	    
 	    Scanner scanner = new Scanner(System.in);
+        // choice variable holds user input of their choice
 	    int choice = scanner.nextInt();
 	    
 	    String[] categories = {"PCs", "Notebooks", "Tablets", "Phones", "Аccessories"};
 	    
 	    try {
+            //print out the user choice
 	        System.out.println(categories[choice]);
 	    } catch(Exception e) {
+            // if there's an error, print out error msg
 	        System.out.println("Wrong Option");
 	    }
 	}
